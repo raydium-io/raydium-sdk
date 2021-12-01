04587fcbc31411a0a6a56fad33cd41d951c70cbd
Tyrion Lannister <connect@raydium.io>
2021-12-01 13:51:41 +0800

    chore: unit test for amount

---
+++ test/entity/amount.test.ts
@@ -0,0 +1,17 @@
+import { Currency, CurrencyAmount } from "../../src/entity";
+
+describe("Test CurrencyAmount", () => {
+  const sol = Currency.SOL;
+
+  it("should works when pass raw lamports", () => {
+    expect(new CurrencyAmount(sol, 1, true).toFixed()).toBe("0.000000001");
+  });
+
+  it("should works when pass UI amount", () => {
+    expect(new CurrencyAmount(sol, 1, false).toFixed()).toBe("1.000000000");
+    expect(new CurrencyAmount(sol, "1", false).toFixed()).toBe("1.000000000");
+    expect(new CurrencyAmount(sol, 1.1, false).toFixed()).toBe("1.100000000");
+    expect(new CurrencyAmount(sol, "1.1", false).toFixed()).toBe("1.100000000");
+    expect(new CurrencyAmount(sol, "1.377865961", false).toFixed()).toBe("1.377865961");
+  });
+});
